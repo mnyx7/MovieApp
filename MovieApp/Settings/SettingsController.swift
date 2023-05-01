f095f0ce34075608cdcabbc2810cf8d4797ab2eb
Minaya Yagubova <minayaqubova@gmail.com>
2023-05-01 20:26:27 +0400

    Title localization

---
+++ MovieApp/Settings/SettingsController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsController.swift
+//  MovieApp
+//
+//  Created by Minaya Yagubova on 01.05.23.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    
+ 
+    @IBAction func azTapped(_ sender: Any) {
+    }
+    @IBAction func ruTapped(_ sender: Any) {
+    }
+    @IBAction func enTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
